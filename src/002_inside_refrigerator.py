4be227cebcf1f6902127aa314dd9a6c509bd0980
y-sugawara-dev <y.sugawara1222@gmail.com>
2026-04-22 22:51:22 +0900

    csvファイルの中を実際にcsvモジュールをつかって取り込み、それを出力してみた。

--- src/002_inside_refrigerator.py
+++ src/002_inside_refrigerator.py
@@ -1,3 +1,5 @@
+import csv
+
 # リストを用いたfor文を実際に書いてみる。
 
 # 画面でユーザが冷蔵庫の中の食材を記入し、それをリストで受け取るとする。
@@ -17,7 +19,42 @@ for index in range(len(inside_refrigerator)):
 # また、for文の書き方も併せて修正が必要だった。
 # 下の文の場合、iはあくまでもリストから文字列を持ってきていてindexを持ってきているわけではない。
 # つまり21行目の[1]でエラーが起こる。
-
+# -----
 # for i in inside_refrigerator:
 #     inside_refrigerator[i] = trim(i)
 #     print(inside_refrigerator[1])
+# -----
+
+
+# 冷蔵庫の中を例として「inside_freezer.csv」というファイルを作成。
+# 実際にファイルの中身をリストとして変数に入れ、出力してみる。
+
+# PyQで学習したことを参考に自分で何もみず書いてみたソースが以下
+# ---------
+# f = open("input/inside_freezer.csv", "r", encoding="utf-8")
+
+# inside_freezer = f.readlines()
+
+# f.close()
+
+# for i in inside_freezer:
+#     print("品名：",i[i.0])
+# ----------
+# 案の定エラー。syntax error。
+# 以下を勉強
+# ①csvを使うならcsvモジュールを入れることを推奨。
+# ②より安全にファイルを管理するならwith open でかくべき
+
+# 以下にAIに教えてもらったことを参考に自分でもう一度書いてみる。
+
+with open("input/inside_freezer.csv", "r",encoding="utf-8") as f:
+    inside_freezer = csv.reader(f)
+    for row in inside_freezer:
+        print("品名：", row[0], "値段", row[1], "賞味期限：", row[2])
+
+# 出力結果は以下のようになり、想定通りに出力することができた。
+# -----
+# 品名： 食パン 値段 200 賞味期限： 2026/4/29
+# 品名： 鶏肉 値段 450 賞味期限： 2026/5/1
+# 品名： ねぎ 値段 80 賞味期限： 2026/4/25
+# -----
